b5c1e867736ec7efb453cff17272cb7ba5bda302
Sam Zhao <zhao.shengjun@gmail.com>
2015-05-30 20:36:29 +1000

    save to mongodb

--- cyzone/pipelines.py
+++ cyzone/pipelines.py
@@ -6,6 +6,35 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class CyzonePipeline(object):
-    def process_item(self, item, spider):
-        return item
+# class CyzonePipeline(object):
+#     def process_item(self, item, spider):
+#         return item
+import pymongo
+from scrapy.conf import settings
+from scrapy.exceptions import DropItem
+from scrapy import log
+
+class MongoDBPipeline(object):
+
+	def __init__(self, arg):
+		# connection = pymongo.MongoClient(
+		# 	settings['MONGODB_SERVER'],
+		# 	settings['MONGODB_PORT']
+		# 	)
+		# db = connection[settings['MONGODB_DB']]
+		# self.collection = db[settings['MONGODB_COLLECTION']]
+		client  = MongoClient
+		db = client['test']
+		collection = db['VCMOVE']
+
+	def process_item(self, item, spider):
+		valid = True
+		for data in item:
+			if not data:
+				valid = False
+				# raise DropItem("Missing {0}!".format(data))
+			if valid:
+				self.collection.insert(dict(item))
+				# log.msg('Added to MongoDB database.', level=log.DEBUG, spider=spider)
+			return item
+		
--- cyzone/settings.py
+++ cyzone/settings.py
@@ -12,6 +12,11 @@ BOT_NAME = 'cyzone'
 
 SPIDER_MODULES = ['cyzone.spiders']
 NEWSPIDER_MODULE = 'cyzone.spiders'
+ITEM_PIPELINES = {'cyzone.pipelines.MongoDBPipeline':1}
+# MONGODB_SERVER = 'localhost'
+# MONGODB_PORT = 27017
+# MONGODB_DB = 'TEST'
+# MONGODB_COLLECTION = 'VCMOVE'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'cyzone (+http://www.yourdomain.com)'
