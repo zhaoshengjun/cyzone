dcd631356e4dd75215da31cd7dfb0e5a204648e6
zhaoshengjun <zhao.shengjun@gmail.com>
2015-05-31 23:41:50 +1000

    change to use crawl-spider

--- cyzone/spiders/cyzone_spider.py
+++ cyzone/spiders/cyzone_spider.py
@@ -1,29 +1,32 @@
-from scrapy.spider import Spider
 from bs4 import BeautifulSoup as bs
 from cyzone.items import CyzoneItem
 from scrapy.http import Request
+import scrapy
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
 
-class CyzoneSpider(Spider):
+class CyzoneSpider(CrawlSpider):
 	name = "cyzone"
 	allowed_domain = []
 	start_urls = ["http://www.cyzone.cn/hotword/%E6%AF%8F%E6%97%A5%E8%9E%8D%E8%B5%84%E6%B8%85%E5%8D%95/"]
+	rules = (
+        Rule(LinkExtractor(allow=r'%E6%AF%8F%E6%97%A5%E8%9E%8D%E8%B5%84%E6%B8%85%E5%8D%95'), callback='parse_item', follow=True),
+    )
 
-
-	def parse(self, response):
+	def parse_item(self, response):
+		# self.log(’Hi, this is an item page! %s’ % response.url)
+		item = CyzoneItem()
 		soup = bs(response.body)
-		items = soup.find_all("div", class_="item-info fl")
-		for item in items:
-			cyzone_item = CyzoneItem()
-			cyzone_item["title"] = item.find("h2", class_="item-tit").get_text()
-			cyzone_item["intro"] = item.find("p", class_="item-intro").get_text()
-			url = item.find("a").get("href")
-			cyzone_item["href"] = url
-			yield scrapy.Request(url, callback=self.parseConent)
-			# print(cyzone_item)
-			yield cyzone_item
-
-	def parseContent(self, response):
-		soup = bs(response.body)
-		title = soup.find('h1').get_text()
-		content = soup.find('div', id='article-content')
-
+		item['title'] = soup.find('h1').get_text()
+		item['content'] = soup.find('div', id='article-content')
+		yield item
+		# url = response.url
+		# items = soup.find_all("div", class_="item-info fl")
+		# for item in items:
+		# 	cyzone_item = CyzoneItem()
+		# 	cyzone_item["title"] = item.find("h2", class_="item-tit").get_text()
+		# 	cyzone_item["intro"] = item.find("p", class_="item-intro").get_text()
+		# 	url = item.find("a").get("href")
+		# 	cyzone_item["href"] = url
+		# 	# print(cyzone_item)
+		# 	yield cyzone_item
