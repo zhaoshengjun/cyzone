251e7c96342386ebb7695c461381c79712c552ae
zhaoshengjun <zhao.shengjun@gmail.com>
2015-06-06 22:45:20 +1000

    scrapy all links

--- cyzone/spiders/cyzone_spider.py
+++ cyzone/spiders/cyzone_spider.py
@@ -1,22 +1,59 @@
 from bs4 import BeautifulSoup as bs
 from cyzone.items import CyzoneItem
 from scrapy.http import Request
-# from scrapy.spider import Spider
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.spider import Spider
+# from scrapy.contrib.spiders import CrawlSpider, Rule
+# from scrapy.contrib.linkextractors import LinkExtractor
 
-class CyzoneSpider(CrawlSpider):
-  name = "cyzone"
-  start_urls = ['http://www.cyzone.cn/hotword/%E6%AF%8F%E6%97%A5%E8%9E%8D%E8%B5%84%E6%B8%85%E5%8D%95']
-  rules = (Rule(LinkExtractor(allow=[r'/\d+']), follow=True),Rule(LinkExtractor(allow=[r'/a\d{8}/\d{6}']), callback='parse_page'))
-  def parse_page(self, response):
-    soup = bs(response.body)
-    cyzone_item = CyzoneItem()
-    cyzone_item["title"] = soup.title
-    cyzone_item["intro"] = soup.find("p",class_="article-introduction").get_text()
-    cyzone_item["href"] = response.url
-    cyzone_item["contents"] = soup.find("div", id="article-content").get_text()
-    return cyzone_item
+class CyzoneSpider(Spider):
+	name = "cyzone"
+	allowed_domain = []
+	# start_urls = ["http://www.cyzone.cn/hotword/%E6%AF%8F%E6%97%A5%E8%9E%8D%E8%B5%84%E6%B8%85%E5%8D%95/"]
+
+	def __init__(self, name=None, **kwargs):
+		urls = []
+		base_url = "http://www.cyzone.cn/hotword/%E6%AF%8F%E6%97%A5%E8%9E%8D%E8%B5%84%E6%B8%85%E5%8D%95/"
+		for i in range(3):
+			url = base_url + str(i)
+			urls.append(url)
+		print(urls)
+		self.start_urls = urls
+
+
+	def parse(self, response):
+		soup = bs(response.body)
+		# generate requests from the page list
+		requests = []
+		items = soup.find_all("div", class_="item-info fl")
+		for item in items:
+			cyzone_item = CyzoneItem()
+			cyzone_item["title"] = item.find("h2", class_="item-tit").get_text()
+			cyzone_item["intro"] = item.find("p", class_="item-intro").get_text()
+			url = item.find("a").get("href")
+			cyzone_item["href"] = url
+			request = Request(url, callback=self.parse_page)
+			request.meta['item'] = cyzone_item
+			requests.append(request)
+		return requests
+
+	def parse_page(self, response):
+		item = response.meta['item']
+		soup = bs(response.body)
+		item['contents'] = soup.find("div", id="article-content").get_text()
+		return item
+
+# class CyzoneSpider(CrawlSpider):
+#   name = "cyzone"
+#   start_urls = ['http://www.cyzone.cn/hotword/%E6%AF%8F%E6%97%A5%E8%9E%8D%E8%B5%84%E6%B8%85%E5%8D%95']
+#   rules = (Rule(LinkExtractor(allow=[r'/\d+']), follow=True),Rule(LinkExtractor(allow=[r'/a\d{8}/\d{6}']), callback='parse_page'))
+#   def parse_page(self, response):
+#     soup = bs(response.body)
+#     cyzone_item = CyzoneItem()
+#     cyzone_item["title"] = soup.title
+#     cyzone_item["intro"] = soup.find("p",class_="article-introduction").get_text()
+#     cyzone_item["href"] = response.url
+#     cyzone_item["contents"] = soup.find("div", id="article-content").get_text()
+#     return cyzone_item
 
 
 # class CyzoneSpider(Spider):
